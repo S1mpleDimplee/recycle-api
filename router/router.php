04c4e848a8ee2830745b5cf0fc8327dec1e53af9
S1mpleDimplee <jaylanovanderveen@gmail.com>
2026-03-16 23:52:48 +0100

    Add new booking functions: GetBookingById and UpdateBookingStatus

--- router/router.php
+++ router/router.php
@@ -105,6 +105,13 @@ switch ($function) {
     case 'getallbookings':
         GetAllBookings($connection);
         break;
+    case 'updatebookingstatus':
+        UpdateBookingStatus($data, $connection);
+        break;
+    case 'getbookingbyid':
+        GetBookingById($data, $connection);
+        break;
+        
     case 'cancelbooking':
         CancelBooking($data, $connection);
         break;
