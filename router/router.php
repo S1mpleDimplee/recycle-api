b95ea339015dcff2f893bc23f4c16cad007ac9aa
RubenOpexis <r.kraan@opexis.nl>
2026-06-01 21:16:47 +0200

    removed incule in router

--- router/router.php
+++ router/router.php
@@ -1,6 +1,4 @@
 ﻿<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
 $allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 if (in_array($origin, $allowedOrigins)) {
