4c1f90d31164cedfc0ec0437d803b9ba7ce45abf
S1mpleDimplee <jaylanovanderveen@gmail.com>
2026-06-01 23:03:23 +0200

    ewqeqw

--- functions/users/GetAllUsers.php
+++ functions/users/GetAllUsers.php
@@ -12,13 +12,18 @@ function GetAllUsers($data, $conn)
     requireAdmin($adminId, $conn);
 
     $result = mysqli_query($conn,
-        "SELECT u.id, u.name, u.username, u.surname, u.email, u.role, u.phonenumber, c.amount AS credits
+        "SELECT u.id, u.name, u.username, u.surname, u.email, u.role, u.phonenumber,
+                c.amount AS credits,
+                (u.profile_img IS NOT NULL AND LENGTH(u.profile_img) > 0) AS has_img
          FROM users u
          LEFT JOIN credits c ON c.id = u.credit_id
          ORDER BY u.id DESC");
 
+    $base = serveBase('serve_profile.php');
     $users = [];
     while ($row = mysqli_fetch_assoc($result)) {
+        $row['profile_img'] = $row['has_img'] ? $base . $row['id'] : null;
+        unset($row['has_img']);
         $users[] = $row;
     }
 
